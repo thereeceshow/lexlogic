85b6aea4ed8314117f3da21a137e80e59cdf5f48
Nathan Wilson <100631201+nwils000@users.noreply.github.com>
2024-07-23 09:04:39 -0400

    Update next.config.mjs
    Removed the unnecessary domain

--- lexlogic-web/next.config.mjs
+++ lexlogic-web/next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['user-images.githubusercontent.com'],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
